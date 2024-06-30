402dc2958ab0500728c5a1ea81516664a576c854
shakiru102 <ashakiru53@gmail.com>
2024-06-30 12:35:32 +0100

    news

--- src/app/components/Landing/News.tsx
+++ src/app/components/Landing/News.tsx
@@ -1,9 +1,33 @@
-import React from 'react'
+import React from 'react';
+import Image from 'next/image'; // If using Next.js, or import any other image component library you use
+import { News as NewsData } from '../data/news'; // Adjust the import path to your actual file structure
 
-function News() {
+const News: React.FC = () => {
   return (
-    <div>News</div>
-  )
-}
+    <main className="py-8 flex flex-col items-center justify-center bg-[#8F1D1F]">
+      <div className='w-[90%]'>
+        <h1 className="text-[36px] text-[#fff] mb-4 text-center font-[600] uppercase">News</h1>
 
-export default News
+        <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-6">
+          {NewsData.map((newsItem, index) => (
+            <div key={index} className="news-item bg-white h-[344px] w-[421-33px] ">
+              <Image
+                src={newsItem.image}
+                alt={newsItem.name}
+                width={421}
+                height={200}
+                // className="rounded mb-4"
+              />
+              <div className='p-4'>
+              <h2 className="text-[16px] font-semibold">{newsItem.name}-{newsItem.report}</h2>
+              <p className="text-[#667085] text-[14px] pt-8">{newsItem.date}</p>
+              </div>
+            </div>
+          ))}
+        </div>
+      </div>
+    </main>
+  );
+};
+
+export default News;
---
+++ src/app/components/data/news.ts
@@ -0,0 +1,41 @@
+import NewsImage from '../../../../public/assets/news.png';
+
+// Define the News array
+export const News = [
+  {
+    date: '17 June 2024',
+    name: 'EKN TRACKSIDE: 2024 Superkarts! USA Pro Tour Spring Nationals',
+    report: 'Sunday Report',
+    image: NewsImage // Use the imported image here
+  },
+  {
+    date: '17 June 2024',
+    name: 'EKN TRACKSIDE: 2024 Superkarts! USA Pro Tour Spring Nationals',
+    report: 'Sunday Report',
+    image: NewsImage // Use the imported image here
+  },
+  {
+    date: '17 June 2024',
+    name: 'EKN TRACKSIDE: 2024 Superkarts! USA Pro Tour Spring Nationals',
+    report: 'Sunday Report',
+    image: NewsImage // Use the imported image here
+  },
+  {
+    date: '17 June 2024',
+    name: 'EKN TRACKSIDE: 2024 Superkarts! USA Pro Tour Spring Nationals',
+    report: 'Sunday Report',
+    image: NewsImage // Use the imported image here
+  },
+  {
+    date: '17 June 2024',
+    name: 'EKN TRACKSIDE: 2024 Superkarts! USA Pro Tour Spring Nationals',
+    report: 'Sunday Report',
+    image: NewsImage // Use the imported image here
+  },
+  {
+    date: '17 June 2024',
+    name: 'EKN TRACKSIDE: 2024 Superkarts! USA Pro Tour Spring Nationals',
+    report: 'Sunday Report',
+    image: NewsImage // Use the imported image here
+  },
+];
