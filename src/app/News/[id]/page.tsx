20adc6e504e7b857d76d05406e37166c2a9b33f9
shakiru102 <ashakiru53@gmail.com>
2024-07-16 11:04:32 +0100

    new-news

--- src/app/News/[id]/page.tsx
+++ src/app/News/[id]/page.tsx
@@ -1,27 +1,38 @@
 // pages/NewsDetail/[id].tsx
 'use client'
-// import { useRouter } from 'next/router';
+import NewsImage from '../../../../public/assets/news.png'
 import { useEffect, useState } from 'react';
 import { useRouter, useParams } from 'next/navigation';
+import Image, { StaticImageData } from 'next/image';
+import Navbar from '@/app/components/Navbar';
+import Footer from '@/app/components/Footer';
+import Latest from '../latest2';
+import Copy from '../../../../public/assets/copy.png'
+import What from '../../../../public/assets/whatsapp.png'
+import Twitter from '../../../../public/assets/twitter.png'
+import Face from '../../../../public/assets/facebook.png'
+import Telegram from '../../../../public/assets/telegram.png'
+import Link from 'next/link';
+import { Button } from 'antd';
 
 
 
 interface NewsItem {
   id: number;
   title: string;
-  image?: string;
   created: string;
   body: string;
+  image: StaticImageData;
 }
 
 const NewsDetail = () => {
   const params = useParams()
- 
+
 
   useEffect(() => {
     console.log(params);
   }, [])
-  
+
   const [newsItem, setNewsItem] = useState<NewsItem | null>(null);
 
   useEffect(() => {
@@ -39,7 +50,7 @@ const NewsDetail = () => {
 
         const data = await response.json();
         console.log(data.data);
-        
+
         setNewsItem(data.data); // Assuming data structure matches NewsItem interface
       } catch (error) {
         console.error('Error fetching news item:', error);
@@ -54,11 +65,53 @@ const NewsDetail = () => {
   }
 
   return (
-    <div>
-      <h1>{newsItem.title}</h1>
-      <p>{newsItem.body}</p>
-      {/* Add more details as needed */}
-    </div>
+    <main >
+      <Navbar />
+      
+      <div className='flex flex-col justify-center items-center my-6'>
+      <div className='w-[85%] mb-6'>
+      <Link href="/News">
+          <Button type="link" className='text-[#101828]'>
+            &larr; Back
+          </Button>
+        </Link>
+      </div>
+        <div className='w-[85%]'>
+          <div className='flex flex-col lg:flex-row gap-6'>
+            <div className='lg:w-[60%]'>
+              <Image alt={newsItem.title} src={newsItem.image || NewsImage} width={500} height={300} className='h-[300px] w-full' />
+            </div>
+            <div className='lg:w-[30%] flex flex-col justify-between lg:ml-10'>
+              <div>
+              <p className='text-[#101828] text-[24px] font-[600]'>{newsItem.title}</p>
+              <p className='text-[#667085] text-[16px]'>{newsItem.created}</p>
+              </div>
+              <div>
+                  <p className='text-[#667085] text-[16px] capitalize'>share</p>
+
+                  <div className='flex gap-2'>
+                  <Image src={Face} alt='' className='w-[32px] h-[32px]'/>
+                  <Image src={Telegram} alt='' className='w-[32px] h-[32px]'/>
+                  <Image src={What} alt='' className='w-[32px] h-[32px]'/>
+                  <Image src={Twitter} alt='' className='w-[32px] h-[32px]'/>
+                    <Image src={Copy} alt='' className='w-[32px] h-[32px]'/>
+                  </div>
+              </div>
+            </div>
+          </div>
+
+          <div className='flex flex-col lg:flex-row mt-20 gap-4'>
+            <div className='lg:w-2/3 text-[#101828] text-[18px]'>
+                {newsItem.body}
+            </div>
+            <div className='lg:w-1/3'>
+              <Latest/>
+            </div>
+          </div>
+        </div>
+      </div>
+      <Footer />
+    </main>
   );
 };
 
--- src/app/News/latest2.tsx
+++ src/app/News/latest2.tsx
@@ -1,33 +1,61 @@
-import React from 'react'
+'use client'
+import React, { useEffect, useState } from 'react'
 import Image from 'next/image';
 import NewsImage from '../../../public/assets/news.png'
 import Link from 'next/link';
-const News = Array.from({ length: 4 }, (_, i) => ({
-    id: i + 1,
-    name: `EKN TRACKSIDE: 2024 Superkarts! USA Pro Tour Spring Nationals`,
-    report: 'Sunday Report',
-    image: NewsImage,
-    date: '17 June 2024',
-   
-  }));
+interface NewsItem {
+  id: number;
+  title: string;
+  image?: string;
+  created: string;
+}
 
 function Latest() {
+  const [news, setNews] = useState<NewsItem[]>([]);
+
+  useEffect(() => {
+    const fetchNews = async () => {
+      try {
+        const response = await fetch('https://xrace.onrender.com/news/news/?category__title=Latest', {
+          headers: {
+            'X-Api-Key': 'ZPuKoTX2CohoPNC8noaiefai4lhLTi5U_PFlNvJraB5bG1mpLbWZqVjuNx6gREUA-f4'
+          }
+        });
+  
+        if (!response.ok) {
+          throw new Error(`HTTP error! Status: ${response.status}`);
+        }
+  
+        const data = await response.json();
+        console.log(data.data, 'confirmed data'); // Log the fetched data
+  
+        // Update events state
+        setNews(data.data);
+        console.log(news);
+  
+      } catch (error) {
+        console.error('Error fetching events:', error);
+      }
+    };
+  
+    fetchNews();
+  }, []);
   return (
     <main className='mb-10'>
     <h1 className="text-align text-black text-4xl font-bold uppercase mb-8">latest news</h1>
    
    <div  className="flex flex-col">
-       {News.map((product) => (
+       {news.map((product) => (
        
          <div key={product.id} >
              <Link href={`/News/${product.id}`}>
-           <div className='flex flex-row gap-2 border-2 lg:border-none p-2 bg-[#FCFCFD]'>
+           <div className='flex flex-col lg:flex-row gap-2  p-2 bg-[#FCFCFD]'>
              <div className=' '>
-               <Image alt={product.name} src={product.image} />
+             <Image alt={product.title} src={product.image || NewsImage} width={500} height={300} className='h-[300px]'/>
              </div>
              <div className='flex flex-col items-left'>
-               <p className='text-[#101828] font-[700] text-[14px] mt-2'>{product.name} - {product.report}</p>
-               <p className=' font-[400] text-[14px] text-[#667085] mt-2'>{product.date}</p>
+               <p className='text-[#101828] font-[700] text-[14px] mt-2'>{product.title}</p>
+               <p className=' font-[400] text-[14px] text-[#667085] mt-2'>{product.created}</p>
             
              </div>
            </div>
